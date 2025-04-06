e7f2cbefa672a50289b8611f37aa035ca5d9a985
Bruno Ferreira <brunofss1985@gmail.com>
2025-04-06 12:35:23 -0300

    aplicação fazendo login e registro perfeitamente

--- src/main/java/com/loja/loja_api/controllers/UserController.java
+++ src/main/java/com/loja/loja_api/controllers/UserController.java
@@ -1,6 +1,9 @@
 package com.loja.loja_api.controllers;
 
+import com.loja.loja_api.domain.user.User;
+import com.loja.loja_api.dto.ResponseDTO;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
     @GetMapping
-    public ResponseEntity<String> getUser() {
-        return ResponseEntity.ok("Sucesso");
+    public ResponseEntity<?> getUser() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var user = (User) authentication.getPrincipal();
+
+        return ResponseEntity.ok(new ResponseDTO(user.getName(), null));
     }
 }
--- src/main/java/com/loja/loja_api/infra/security/SecurityConfig.java
+++ src/main/java/com/loja/loja_api/infra/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.loja.loja_api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,21 +8,26 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // Não usa mais .cors(), o CORS é automaticamente ativado
-        // quando há um CorsConfigurationSource no contexto
         http
+                .cors(withDefaults()) // Isso garante que o CorsConfigurationSource será usado
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
@@ -31,11 +37,11 @@ public class SecurityConfig {
         return http.build();
     }
 
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Frontend Angular
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(true);
@@ -46,9 +52,9 @@ public class SecurityConfig {
         return source;
     }
 
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
-
