41305fc50e5f3f28ced673ecd328699eef80b724
Bruno Ferreira <brunofss1985@gmail.com>
2025-04-08 16:46:58 -0300

    implantado guarda de rota na aplicacao, diferenciando ADMIN e USER

--- src/main/java/com/loja/loja_api/infra/security/TokenService.java
+++ src/main/java/com/loja/loja_api/infra/security/TokenService.java
@@ -14,9 +14,11 @@ import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
+
     @Value("${api.security.token.secret}")
     private String secret;
 
+    // Gera o token JWT com subject (email), userType e tempo de expiração
     public String generateToken(User user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
@@ -24,27 +26,48 @@ public class TokenService {
             return JWT.create()
                     .withIssuer("login-auth-api")
                     .withSubject(user.getEmail())
-                    .withClaim("userType", user.getUserType().name())
-                    .withExpiresAt(this.generateExpirationDate())
+                    .withClaim("userType", user.getUserType().name()) // Adiciona o tipo de usuário
+                    .withExpiresAt(generateExpirationDate())
                     .sign(algorithm);
+
         } catch (JWTCreationException exception) {
-            throw new RuntimeException("Error while authenticating");
+            throw new RuntimeException("Erro ao gerar o token JWT", exception);
         }
     }
 
+    // Valida o token e retorna o email (subject)
     public String validateToken(String token) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
+
             return JWT.require(algorithm)
                     .withIssuer("login-auth-api")
                     .build()
                     .verify(token)
-                    .getSubject();
+                    .getSubject(); // Retorna o email do usuário
+
         } catch (JWTVerificationException exception) {
             return null;
         }
     }
 
+    // Método para extrair o userType do token
+    public String getUserTypeFromToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getClaim("userType").asString();
+
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+
+    // Define a expiração do token (2 horas a partir de agora)
     private Instant generateExpirationDate() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
