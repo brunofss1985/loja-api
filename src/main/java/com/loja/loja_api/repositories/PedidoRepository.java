6a9369e6ceffe536afb6becdaac6659f15005e0b
Bruno Ferreira <brunofss1985@gmail.com>
2025-08-19 17:46:35 -0300

    refatorando os codigos

--- src/main/java/com/loja/loja_api/repositories/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.loja.loja_api.repositories;
-
-import com.loja.loja_api.model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
